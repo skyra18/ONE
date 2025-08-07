df16e91d696aab4239e16f695016ce28408e7ae7
Skyra <eduarda.alves@aluno.ufabc.edu.br>
2025-08-06 22:52:06 -0300

    Update app.js

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 alert('Boas vindas ao jogo do número secreto');
-let numeroMaximo = 500;
+let numeroMaximo = 200;
 let numeroSecreto = parseInt(Math.random() * numeroMaximo + 1);
 console.log(numeroSecreto);
 let chute;
